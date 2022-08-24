58c09a20244d90f2d6c84b913ee01ed257572e9a
Savio Ferreira <ferreirasavio39@gmail.com>
2022-08-23 22:47:11 -0300

    Other componets

---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,102 @@
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+    return (
+        <div className="dsmeta-card">
+            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-font-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+                <div className="dsmeta-font-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+            </div>
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">10/10/2022</td>
+                            <td>Roronoa</td>
+                            <td className="show992">3</td>
+                            <td className="show992">5</td>
+                            <td>R$ 1.600000.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">10/10/2022</td>
+                            <td>Roronoa</td>
+                            <td className="show992">3</td>
+                            <td className="show992">5</td>
+                            <td>R$ 1.600000.00</td>
+                            <td>
+                            <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">10/10/2022</td>
+                            <td>Roronoa</td>
+                            <td className="show992">3</td>
+                            <td className="show992">5</td>
+                            <td>R$ 1.600000.00</td>
+                            <td>
+                            <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">10/10/2022</td>
+                            <td>Roronoa</td>
+                            <td className="show992">3</td>
+                            <td className="show992">5</td>
+                            <td>R$ 1.600000.00</td>
+                            <td>
+                            <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">10/10/2022</td>
+                            <td>Roronoa</td>
+                            <td className="show992">3</td>
+                            <td className="show992">5</td>
+                            <td>R$ 1.600000.00</td>
+                            <td>
+                            <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default SalesCard
